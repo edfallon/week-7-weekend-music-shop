f3e7874ddc9e627c90277fbc1919187375db8f61
Edward Fallon <eddie_fallon@hotmail.com>
2018-05-10 23:19:12 +0100

    added abstract class Instrument and created getters

---
+++ src/main/java/Instrument.java
@@ -0,0 +1,24 @@
+public abstract class Instrument {
+
+    private String material;
+    private String colour;
+    private String family;
+
+    public Instrument(String material, String colour, String family) {
+        this.material = material;
+        this.colour = colour;
+        this.family = family;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+}
