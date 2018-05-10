581d29ed5bd1fddbaf0bcc9c017a96a3629ada21
Edward Fallon <eddie_fallon@hotmail.com>
2018-05-11 00:03:44 +0100

    created Piano class and added getters. First piano test passing

---
+++ src/main/java/Piano.java
@@ -0,0 +1,25 @@
+public class Piano extends Instrument{
+
+    private int numberOfKeys;
+    private int numberOfPedals;
+    private String style;
+
+    public Piano(String material, String colour, String family, int numberOfKeys, int numberOfPedals, String style) {
+        super(material, colour, family);
+        this.numberOfKeys = numberOfKeys;
+        this.numberOfPedals = numberOfPedals;
+        this.style = style;
+    }
+
+    public int getNumberOfKeys() {
+        return numberOfKeys;
+    }
+
+    public int getNumberOfPedals() {
+        return numberOfPedals;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+}
