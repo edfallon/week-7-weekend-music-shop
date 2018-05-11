714ef1c1459d55479c6194cbba035770ac4823c2
Edward Fallon <eddie_fallon@hotmail.com>
2018-05-11 11:48:25 +0100

    created passing tests to add and remove items to stock and calculated total possible profit

--- src/main/java/shop/Shop.java
+++ src/main/java/shop/Shop.java
@@ -1,6 +1,8 @@
 package shop;
 
+import instruments.Guitar;
 import instruments.ISell;
+import otherItems.Item;
 
 import java.util.ArrayList;
 
@@ -14,5 +16,23 @@ public class Shop {
 
     public int stockCount(){
         return this.stockArray.size();
+    }
+
+
+    public void addISellItem(ISell iSell) {
+        this.stockArray.add(iSell);
+    }
+
+    public void removeISellItem(ISell iSell) {
+        this.stockArray.remove(iSell);
+    }
+
+    public int getTotalPossibleProfit() {
+        int total = 0;
+        for (ISell isell : stockArray){
+             total += isell.calculateMarkup();
+        }
+
+        return total;
     }
 }
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,3 +1,4 @@
+import instruments.Guitar;
 import org.junit.Before;
 import org.junit.Test;
 import shop.Shop;
@@ -7,14 +8,35 @@ import static org.junit.Assert.assertEquals;
 public class ShopTest {
 
     Shop shop1;
+    Guitar guitar1;
 
     @Before
     public void setUp() throws Exception {
         shop1 = new Shop();
+        guitar1 = new Guitar(100, 200, "Wood", "Black", "String", 6, "Acoustic");
     }
 
     @Test
     public void checkStockArrayStartsEmpty() {
         assertEquals(0, shop1.stockCount());
+    }
+
+    @Test
+    public void canAddISellItemToStockArray() {
+        shop1.addISellItem(guitar1);
+        assertEquals(1, shop1.stockCount());
+    }
+
+    @Test
+    public void canRemoveISellItemFromStock() {
+        shop1.addISellItem(guitar1);
+        shop1.removeISellItem(guitar1);
+        assertEquals(0, shop1.stockCount());
+    }
+
+    @Test
+    public void canGetTotalPossibleProfit() {
+        shop1.addISellItem(guitar1);
+        assertEquals(100, shop1.getTotalPossibleProfit());
     }
 }
