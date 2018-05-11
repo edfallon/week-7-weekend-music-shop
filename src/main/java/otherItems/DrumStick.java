5a99c5dda1d55318772d3b135266242f4b93c116
Edward Fallon <eddie_fallon@hotmail.com>
2018-05-11 10:51:55 +0100

    passing test for drumstick get buying price

---
+++ src/main/java/otherItems/DrumStick.java
@@ -0,0 +1,10 @@
+package otherItems;
+
+public class DrumStick extends Item {
+
+
+
+    public DrumStick(int buyingPrice, int sellingPrice, String description) {
+        super(buyingPrice, sellingPrice, description);
+    }
+}
--- src/main/java/otherItems/Item.java
+++ src/main/java/otherItems/Item.java
@@ -1,4 +1,26 @@
 package otherItems;
 
-public class Item {
+public abstract class Item {
+
+    private int buyingPrice;
+    private int sellingPrice;
+    private String description;
+
+    public Item(int buyingPrice, int sellingPrice, String description) {
+        this.buyingPrice = buyingPrice;
+        this.sellingPrice = sellingPrice;
+        this.description = description;
+    }
+
+    public int getBuyingPrice() {
+        return buyingPrice;
+    }
+
+    public int getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
